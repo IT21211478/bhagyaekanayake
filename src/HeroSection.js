9539c1ba50927a61b225bdd5d909a8470059215e
IT21211478 <it21211478@my.sliit.lk>
2025-04-05 12:38:58 +0530

    Added basic SEO to portfolio

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { ReactTyped as Typed } from "react-typed"; // Correctly import react-typed
 import "./HeroSection.css";
 import profilePhoto from "./data/propic.jpg"; // Replace with your profile photo path
@@ -7,6 +8,46 @@ import resume from "./data/BhagyaEkanayake.pdf"; // Replace with your resume fil
 
 function HeroSection() {
   return (
+    
+    <>
+      <Helmet>
+        <title>Bhagya Ekanayake | Portfolio</title>
+        <meta
+          name="description"
+          content="Welcome to Bhagya Ekanayake's developer portfolio. Explore projects, skills, and resume."
+        />
+        <meta
+          name="keywords"
+          content="Bhagya Ekanayake, Software Developer, Portfolio, Full Stack Developer, Internship"
+        />
+        <meta name="author" content="Bhagya Ekanayake" />
+
+        {/* Social media preview (Open Graph) */}
+        <meta property="og:title" content="Bhagya Ekanayake | Portfolio" />
+        <meta
+          property="og:description"
+          content="Explore Bhagya's work, resume, and technical skills."
+        />
+        <meta
+          property="og:image"
+          content="https://bhagyaekanayake.me/propic2.jpg"
+        />
+        <meta property="og:url" content="https://bhagyaekanayake.me/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Cards */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Bhagya Ekanayake | Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Explore Bhagya's work, resume, and technical skills."
+        />
+        <meta
+          name="twitter:image"
+          content="https://bhagyaekanayake.me/propic2.jpg"
+        />
+      </Helmet>
+
     <section className="hero">
       <div className="hero-content">
       <h1 className="inter-hello">Hello, I'm</h1>
@@ -42,6 +83,7 @@ function HeroSection() {
         <img src={profilePhoto} alt="Bhagya Ekanayake" />
       </div>
     </section>
+  </>
     
   );
 }
